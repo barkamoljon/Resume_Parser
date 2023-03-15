80234388cdeaeff5c328d50dc24847ac20a90028
Barkamol Urinboev <97020905+barkamoljon@users.noreply.github.com>
2023-03-15 19:38:43 +0500

    Update App.py

--- App.py
+++ App.py
@@ -16,8 +16,8 @@ import pymysql
 import pafy
 import plotly.express as px
 nltk.data.path.append('nltk_data')
-nltk.download('stopwords', download_dir='./nltk_data/')
-nltk.download('punkt', download_dir='./nltk_data/')
+nltk.download('stopwords', download_dir='././nltk_data/')
+nltk.download('punkt', download_dir='././nltk_data/')
 
 def fetch_yt_video(link):
     video = pafy.new(link)
